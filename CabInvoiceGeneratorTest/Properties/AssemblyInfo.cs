e595c63cf350e3c1edda01045b05c815fab60a6d
pranali2808 <ranepranali1997@gmail.com>
2022-05-11 21:21:10 +0530

    Calculate total fare for the journey

---
+++ CabInvoiceGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2a42c75-6494-43e9-b2ab-7d14e3aa941b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CabInvoiceGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabInvoiceGeneratorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public CabInvoGenerator generateNormalFare;
+        [TestInitialize]
+        public void Setup()
+        {
+            generateNormalFare = new CabInvoGenerator(RideType.NORMAL);
+        }
+        [TestMethod]
+        public void GivenProperDistanceAndTimeShouldResturnFare()
+        {
+            double expected = 160;
+            int time = 10;
+            double distance = 15;
+            double actual = generateNormalFare.CalculateFare(time, distance);
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void GivenImproperTimeDistanceShouldThrowException()
+        {
+            var invalidTimeException = Assert.ThrowsException<CabInvoGeneratorException>(() => generateNormalFare.CalculateFare(0, 5));
+            Assert.AreEqual(CabInvoGeneratorException.ExceptionType.INVALID_TIME, invalidTimeException.exceptionType);
+            var invalidDistanceException = Assert.ThrowsException<CabInvoGeneratorException>(() => generateNormalFare.CalculateFare(12, 0));
+            Assert.AreEqual(CabInvoGeneratorException.ExceptionType.INVALID_DISTANCE, invalidDistanceException.exceptionType);
+        }
+    }
+}
+
